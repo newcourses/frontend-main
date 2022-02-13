fc5228c19710abcfd27522aa441c565a16ffd6a4
alek <cheprasov.dev@gmail.com>
2022-02-13 20:44:57 +0700

    refactoring Header

--- src/layouts/Main/components/Header/Header.jsx
+++ src/layouts/Main/components/Header/Header.jsx
@@ -1,33 +1,36 @@
 import React from 'react';
 import { DownOutlined } from '@ant-design/icons';
 import Link from 'next/link';
+import { Button } from 'antd';
+import cn from 'classnames';
 import css from './Header.module.scss';
 import Logo from '../../../../components/Logo/Logo';
-import OvalButton from '../../../../components/OvalButton/OvalButton';
 
 function Header() {
   return (
     <header className={css.container}>
       <Logo />
       <nav className={css.nav}>
-        <OvalButton classNames={[css.link]}>
-          Все категории курсов
-          <DownOutlined style={{ marginLeft: '1rem' }} />
-        </OvalButton>
-        <ul className={css.linksWrapper}>
+        <Button type="primary" size="large" style={{ height: '4rem' }}>
+          <span className={css.textButton}>
+            Все категории курсов
+            <DownOutlined style={{ marginLeft: '1rem' }} />
+          </span>
+        </Button>
+        <ul className={css.linksContainer}>
           <li>
             <Link href="/">
-              <a className={css.link}>Some link</a>
+              <a className={cn(css.linkWrapper, css.link)}>Some link</a>
             </Link>
           </li>
           <li>
             <Link href="/" passHref>
-              <a className={css.link}>Some link</a>
+              <a className={cn(css.linkWrapper, css.link)}>Some link</a>
             </Link>
           </li>
           <li>
             <Link href="/" passHref>
-              <a className={css.link}>Some link</a>
+              <a className={cn(css.linkWrapper, css.link)}>Some link</a>
             </Link>
           </li>
         </ul>
