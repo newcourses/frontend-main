209f4ef0c414f72812dada1751dfd35c614201fa
alek <cheprasov.dev@gmail.com>
2022-02-13 20:48:35 +0700

    added CoursesTableCell

---
+++ src/components/CoursesTableCell/CourseInfo/CourseInfo.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import css from './CourseInfo.module.scss';
+
+export function CourseInfo({ title, link }) {
+  return (
+    <div className={css.wrapper}>
+      <span className={css.title}>{title}</span>
+      <a
+        className={css.link}
+        href={link}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Ссылка на курс
+      </a>
+    </div>
+  );
+}
+
+CourseInfo.propTypes = {
+  title: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired,
+};
+
+export default React.memo(CourseInfo);
