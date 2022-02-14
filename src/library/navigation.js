45b5ad104fa0b9314ebeff4217826265687bc1a1
alek <cheprasov.dev@gmail.com>
2022-02-14 11:41:39 +0700

    added rout coursesCategory

--- src/library/navigation.js
+++ src/library/navigation.js
@@ -1,5 +1,10 @@
 export default {
   home: { link: '/', value: 'На главную' },
+  coursesCategory: {
+    link: '/courses-category/[id]',
+    as: (id) => `/courses-category/${id}`,
+    value: 'На главную',
+  },
   about: { link: '/about', value: 'О нас' },
   contacts: { link: '/contacts', value: 'Контакты' },
 };
