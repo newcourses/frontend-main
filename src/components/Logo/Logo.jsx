641981043a2c219df0c5ab01a1e093bab4c645c1
alek <cheprasov.dev@gmail.com>
2022-02-15 11:27:34 +0700

    upgrade Logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import Link from 'next/link';
+import PropTypes from 'prop-types';
 import css from './Logo.module.scss';
 import NAVIGATION from '../../library/navigation';
 
-function Logo() {
+function Logo({ logo }) {
   return (
-    <Link href={NAVIGATION.home} passHref>
+    <Link href={NAVIGATION.home.link} passHref>
       <a>
-        <img
-          className={css.logo}
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Coca-Cola_logo.svg/1200px-Coca-Cola_logo.svg.png"
-          alt="LOGO"
-        />
+        <img className={css.logo} src={logo.src} alt="logo" />
       </a>
     </Link>
   );
 }
 
-Logo.propTypes = {};
+Logo.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  logo: PropTypes.object.isRequired,
+};
 
 export default React.memo(Logo);
