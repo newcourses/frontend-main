5b779247a2ce11ea8bfdfd2357c45a753074ebb9
Alexandr Cheprasov <cheprasov.dev@gmail.com>
2022-02-17 12:37:29 +0700

    created SchoolsInfo

---
+++ src/containers/SchoolsInfo/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import css from './index.module.scss';
+import InfoSchoolAndCourses from '../InfoSchoolAndCourses/InfoSchoolAndCourses';
+
+function SchoolsInfo({ schools, title, description }) {
+  return (
+    <div>
+      <h3 className={css.title}>{title}</h3>
+      <div className={css.description}>{description}</div>
+      {schools.map((elem) => (
+        <InfoSchoolAndCourses {...elem} />
+      ))}
+    </div>
+  );
+}
+
+SchoolsInfo.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  schools: [],
+};
+SchoolsInfo.defaultProps = {
+  schools: [],
+};
+
+export default React.memo(SchoolsInfo);
