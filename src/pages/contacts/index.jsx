f73e41ef4afb39560f2ab6bc9eae9cb598f3b543
Alexandr Cheprasov <cheprasov.dev@gmail.com>
2022-02-17 12:33:36 +0700

    created contacts page

---
+++ src/pages/contacts/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Main from '../../layouts/Main/Main';
+import useVisibleDrawer from '../../hooks/useVisibleDrawer';
+import { ICourseCategories } from '../../propTypes';
+import getCategories from '../../controllers/getCategories';
+
+function Contacts({ categories }) {
+  const { visibleDrawer, setVisibleDrawer } = useVisibleDrawer();
+  return (
+    <Main
+      visibleDrawer={visibleDrawer}
+      setVisibleDrawer={setVisibleDrawer}
+      categories={categories.data}
+    >
+      <main>Contacts</main>
+    </Main>
+  );
+}
+
+Contacts.propTypes = {
+  categories: ICourseCategories,
+};
+
+Contacts.defaultProps = {
+  categories: {
+    data: [],
+  },
+};
+
+export async function getStaticProps() {
+  const categories = await getCategories();
+
+  return {
+    props: { categories },
+  };
+}
+
+export default React.memo(Contacts);
