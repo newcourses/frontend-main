0ec1c2bbb1a89c279bd6d35544ff0c33e05c1f2d
Alexandr Cheprasov <cheprasov.dev@gmail.com>
2022-02-17 12:40:39 +0700

    deleted rubber

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,48 +1,12 @@
 import '../styles/globals.css';
-import React, { useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import 'moment/locale/ru';
 import moment from 'moment';
 
 moment.locale('ru');
 
-function changeSize(config) {
-  const { baseSize } = config;
-  const { baseWidth } = config;
-  const { widthLimit } = config;
-
-  const html = document.documentElement;
-  let width = html.clientWidth || document.body.clientWidth;
-  if (widthLimit) {
-    width = Math.min(width, widthLimit);
-  }
-
-  const currentSize = (width / baseWidth) * baseSize;
-  html.style.fontSize = `${currentSize}px`;
-}
-
-const elasticConfig = {
-  desktop: {
-    baseSize: 10,
-    baseWidth: 1440,
-    widthLimit: 1660,
-  },
-};
-
-function useElasticAdaptive(device) {
-  useEffect(() => {
-    const update = () => changeSize(elasticConfig[device]);
-
-    update();
-    window.addEventListener('resize', update);
-    return () => {
-      window.removeEventListener('resize', update);
-    };
-  }, [device]);
-}
-
 function MyApp({ Component, pageProps }) {
-  useElasticAdaptive('desktop');
   return <Component {...pageProps} />;
 }
 
