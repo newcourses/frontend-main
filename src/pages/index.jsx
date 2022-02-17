239c846621e90ab7992c535580faf90e6a65e89d
Alexandr Cheprasov <cheprasov.dev@gmail.com>
2022-02-17 12:40:54 +0700

    refactoring home page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,20 +1,23 @@
-import React, { useState } from 'react';
-import qs from 'qs';
-import PropTypes from 'prop-types';
+import React from 'react';
 import Main from '../layouts/Main/Main';
 import CourseCategories from '../containers/CourseCategories/CourseCategories';
-import { api } from '../utils/axiosInstances';
 import { ICourseCategories } from '../propTypes';
 import MainBanner from '../containers/MainBanner/MainBanner';
+import useVisibleDrawer from '../hooks/useVisibleDrawer';
+import getCategories from '../controllers/getCategories';
 
-function Home({ data }) {
-  const [visibleDrawer, setVisibleDrawer] = useState(false);
+function Home({ categories }) {
+  const { visibleDrawer, setVisibleDrawer } = useVisibleDrawer();
   return (
-    <Main visibleDrawer={visibleDrawer} setVisibleDrawer={setVisibleDrawer}>
+    <Main
+      visibleDrawer={visibleDrawer}
+      setVisibleDrawer={setVisibleDrawer}
+      categories={categories.data}
+    >
       <main>
         <MainBanner setVisibleDrawer={setVisibleDrawer} />
         <CourseCategories
-          categories={data}
+          categories={categories.data}
           setVisibleDrawer={setVisibleDrawer}
         />
       </main>
@@ -23,23 +26,20 @@ function Home({ data }) {
 }
 
 Home.propTypes = {
-  data: PropTypes.arrayOf(ICourseCategories.isRequired),
+  categories: ICourseCategories,
 };
 
 Home.defaultProps = {
-  data: [],
+  categories: {
+    data: [],
+  },
 };
 
 export async function getStaticProps() {
-  const query = qs.stringify(
-    { populate: ['subcategories'] },
-    { encodeValuesOnly: true },
-  );
-
-  const { data } = await api.get(`/categories?${query}`);
+  const categories = await getCategories();
 
   return {
-    props: data,
+    props: { categories },
   };
 }
 
