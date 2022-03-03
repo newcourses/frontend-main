08e1423a13ad4e24af10ce7de15504c7f845d88a
Alexandr Cheprasov <cheprasov.dev@gmail.com>
2022-03-03 14:03:42 +0700

    created prepareSchoolReviews

---
+++ src/helpers/preparersData/prepareSchoolReviews.js
@@ -0,0 +1,37 @@
+function countReactions(reactions = []) {
+  const likes = reactions.filter((reaction) => reaction.attributes.indicator);
+  const dislikes = reactions.filter(
+    (reaction) => !reaction.attributes.indicator,
+  );
+
+  return {
+    likes: likes.length,
+    dislikes: dislikes.length,
+  };
+}
+function prepareSchoolReviews(reviews) {
+  return reviews.data.map((review) => {
+    const countReactionReview = countReactions(
+      review.attributes.reactions.data,
+    );
+    review.attributes.likes = countReactionReview.likes;
+    review.attributes.dislikes = countReactionReview.dislikes;
+
+    review.attributes.comments.data = review.attributes.comments.data.map(
+      (comment) => {
+        const countReactionComment = countReactions(
+          comment.attributes.reactions.data,
+        );
+        comment.attributes.likes = countReactionComment.likes;
+        comment.attributes.dislikes = countReactionComment.dislikes;
+        delete comment.attributes.reactions;
+        return comment;
+      },
+    );
+
+    delete review.attributes.reactions;
+    return review;
+  });
+}
+
+export default prepareSchoolReviews;
