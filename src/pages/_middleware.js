eb8bda261175aca56b6310bc867fd770a9c6f811
Alexandr Cheprasov <cheprasov.dev@gmail.com>
2022-03-03 14:07:49 +0700

    added middleware for update visitor

---
+++ src/pages/_middleware.js
@@ -0,0 +1,31 @@
+import { NextResponse } from 'next/server';
+import { v4 as uuidv4 } from 'uuid';
+import { fetchCmsApi } from '../utils/fetchInstances';
+
+const VISITOR_UID = 'visitor-uid';
+
+async function setVisitorCookie(request, response) {
+  const cookie = request.cookies[VISITOR_UID];
+
+  if (!cookie) {
+    const uid = uuidv4();
+    const { data } = await fetchCmsApi({
+      url: '/visitors',
+      method: 'POST',
+      body: {
+        data: {
+          uid,
+        },
+      },
+    });
+
+    response.cookie(VISITOR_UID, data.attributes.uid);
+  }
+}
+
+// eslint-disable-next-line import/prefer-default-export
+export async function middleware(req) {
+  const res = NextResponse.next();
+  await setVisitorCookie(req, res);
+  return res;
+}
