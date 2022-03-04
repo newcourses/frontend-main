a6a10658e3f0a452632d9e2b63b4d73d90fced42
Alexandr Cheprasov <cheprasov.dev@gmail.com>
2022-03-04 12:23:02 +0700

    created BackTopButton

---
+++ src/components/BackTopButton/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BackTop } from 'antd';
+import cn from 'classnames';
+import { VerticalAlignTopOutlined } from '@ant-design/icons';
+import css from './index.module.scss';
+
+function BackTopButton() {
+  return (
+    <BackTop>
+      <div className={cn(css.button, css.backTop)}>
+        <VerticalAlignTopOutlined className={css.backTopIcon} />
+      </div>
+    </BackTop>
+  );
+}
+
+BackTopButton.propTypes = {};
+BackTopButton.defaultProps = {};
+
+export default React.memo(BackTopButton);
