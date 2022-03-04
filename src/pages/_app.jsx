19f57c142270eea00cacbb4f6259a5dfd7450345
Alexandr Cheprasov <cheprasov.dev@gmail.com>
2022-03-04 12:23:10 +0700

    added BackTopButton

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,11 +3,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import 'moment/locale/ru';
 import moment from 'moment';
+import BackTopButton from '../components/BackTopButton';
 
 moment.locale('ru');
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <BackTopButton />
+    </>
+  );
 }
 
 MyApp.propTypes = {
