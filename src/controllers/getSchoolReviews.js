8e6b4a2bbc8ba9c85ad6b33b642bd87372540f2e
Alexandr Cheprasov <cheprasov.dev@gmail.com>
2022-03-04 12:24:22 +0700

    upgrade getSchoolReviews

--- src/controllers/getSchoolReviews.js
+++ src/controllers/getSchoolReviews.js
@@ -1,10 +1,15 @@
 import qs from 'qs';
 import { cmsApi } from '../utils/axiosInstances';
+import { COUNT_DEFAULT_SIZE_PAGE } from '../library/constants';
 
-export default async function getSchoolReviews({ code }) {
+export default async function getSchoolReviews({
+  code,
+  sort = 'date:desc',
+  pagination = { pageSize: COUNT_DEFAULT_SIZE_PAGE },
+}) {
   const query = qs.stringify(
     {
-      sort: ['date:asc'],
+      sort,
       populate: [
         'school',
         'reviewer',
@@ -14,16 +19,15 @@ export default async function getSchoolReviews({ code }) {
         'comments.reactions',
       ],
       filters: {
-        // id: 16851,
         school: {
           code,
         },
       },
+      pagination,
     },
     { encodeValuesOnly: true },
   );
 
   const { data } = await cmsApi.get(`/reviews?${query}`);
-
   return data;
 }
