26f319321d6e5729b9f6017df7b8d7421d01ab3c
Alexandr Cheprasov <cheprasov.dev@gmail.com>
2022-03-19 18:49:22 +0700

    upgraded router for create reaction

--- src/api/controllers/reactions/create/index.js
+++ src/api/controllers/reactions/create/index.js
@@ -6,7 +6,7 @@ export default async (req, res) => {
   const visitorUid = req.cookies[VISITOR_UID];
 
   if (!visitorUid) {
-    throw ApiError('Отсутствует VISITOR_UID', 400);
+    throw ApiError('Отсутствует VISITOR_UID, обновите страницу', 404);
   }
 
   const body = await ReactionsServices.create({ uid: visitorUid, ...req.body });
--- src/api/services/reactions/create/index.js
+++ src/api/services/reactions/create/index.js
@@ -1,3 +1,63 @@
+import qs from 'qs';
+import { cmsApi } from '../../../../utils/axiosInstances';
+import { ApiError } from '../../../../helpers';
+
+const MISSING_VISITOR =
+  'Ошибка! Обновите страницу! \nЕсли ошибка сохранится почитстите куки и кеш';
+
+async function getVisitor(uid) {
+  const queryVisitor = qs.stringify(
+    {
+      filters: {
+        uid,
+      },
+    },
+    { encodeValuesOnly: true },
+  );
+  const { data: visitors } = await cmsApi.get(`/visitors?${queryVisitor}`);
+  return visitors?.data?.[0];
+}
+
+async function canCreateReaction({ visitor, review, comment }) {
+  const queryReactions = qs.stringify(
+    {
+      filters: {
+        visitor,
+        $or: [{ review }, { comment }],
+      },
+    },
+    { encodeValuesOnly: true },
+  );
+
+  const { data: reactions } = await cmsApi.get(`/reactions?${queryReactions}`);
+
+  return reactions?.data?.length === 0;
+}
+
 export default async (body) => {
-  return body;
+  const visitor = await getVisitor(body.uid);
+  if (!visitor) {
+    throw ApiError(MISSING_VISITOR, 409);
+  }
+
+  const isCreateReaction = await canCreateReaction({
+    visitor: visitor.id,
+    review: body.review,
+    comment: body.comment,
+  });
+
+  if (!isCreateReaction) {
+    throw ApiError('Вы уже поставили оценку', 409);
+  }
+
+  await cmsApi.post(`/reactions`, {
+    data: {
+      visitor: visitor.id,
+      review: body.review,
+      comment: body.comment,
+      indicator: body.indicator,
+    },
+  });
+
+  return 'ok';
 };
