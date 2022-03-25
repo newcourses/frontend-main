dfd8d4e35bafb19a7ea3c52930f63b042dacacf7
Alexandr Cheprasov <cheprasov.dev@gmail.com>
2022-03-25 11:46:47 +0700

    FIX: fixed error with create comment

--- src/api/controllers/comments/create/index.js
+++ src/api/controllers/comments/create/index.js
@@ -25,7 +25,7 @@ export default async (req, res) => {
   const result = await CommentsServices.create({
     text: body.text,
     reviewId: body.reviewId,
-    reviewerId: reviewer.id,
+    reviewerId: reviewer.data.id,
   });
 
   return res.status(201).json(result);
