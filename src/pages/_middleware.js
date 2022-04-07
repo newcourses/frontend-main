9e84d0580fee814a8f74e48824a62c0f9e6b60be
Alexandr Cheprasov <cheprasov.dev@gmail.com>
2022-04-07 09:25:27 +0700

    TEST: test

--- src/pages/_middleware.js
+++ src/pages/_middleware.js
@@ -1,30 +1,30 @@
-// import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 import { NextResponse } from 'next/server';
-// import { VISITOR_UID } from '../library/constants';
+import { VISITOR_UID } from '../library/constants';
 // import { fetchCmsApi } from '../utils/fetchInstances';
 
-// async function setVisitorCookie(request, response) {
-//   const cookie = request.cookies[VISITOR_UID];
-//
-//   if (!cookie) {
-//     const uid = uuidv4();
-//     const { data } = await fetchCmsApi({
-//       url: '/visitors',
-//       method: 'POST',
-//       body: {
-//         data: {
-//           uid,
-//         },
-//       },
-//     });
-//
-//     response.cookie(VISITOR_UID, data.attributes.uid);
-//   }
-// }
+async function setVisitorCookie(request, response) {
+  const cookie = request.cookies[VISITOR_UID];
+
+  if (!cookie) {
+    const uid = uuidv4();
+    // const { data } = await fetchCmsApi({
+    //   url: '/visitors',
+    //   method: 'POST',
+    //   body: {
+    //     data: {
+    //       uid,
+    //     },
+    //   },
+    // });
+
+    response.cookie(VISITOR_UID, uid);
+  }
+}
 
 // eslint-disable-next-line import/prefer-default-export
-export async function middleware() {
+export async function middleware(req) {
   const res = NextResponse.next();
-  // await setVisitorCookie(req, res);
+  await setVisitorCookie(req, res);
   return res;
 }
