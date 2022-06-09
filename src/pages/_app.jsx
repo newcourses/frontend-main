749b50f847357d187101615858e5a4c5546b8225
Alexandr Cheprasov <cheprasov.dev@gmail.com>
2022-06-09 09:20:06 +0700

    [ADD]: added yandex metrika

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,11 +2,17 @@ import 'styles/globals.scss';
 import React from 'react';
 import 'moment/locale/ru';
 import moment from 'moment';
+import YaMetrika from 'components/YaMetrika';
 
 moment.locale('ru');
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <YaMetrika />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
