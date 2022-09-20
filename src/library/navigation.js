79025a438ac5f934f1ffc30bde21796e9f20607e
Alexander Cheprasov <cheprasov.dev@gmail.com>
2022-09-20 19:11:37 +0300

    main: added navigation link

--- src/library/navigation.js
+++ src/library/navigation.js
@@ -4,8 +4,17 @@ export default {
     link: '/courses-category/[code]',
     as: (id) => `/courses-category/${id}`,
   },
+  freeCoursesCategory: {
+    link: '/free-courses-category/[code]',
+    as: (id) => `/free-courses-category/${id}`,
+  },
   about: { link: '/about', caption: 'О нас' },
   contacts: { link: '/contacts', caption: 'Контакты' },
+  freeCourses: {
+    link: '/free-courses-category',
+    caption: 'Бесплатные курсы',
+  },
+
   schoolsList: { link: '/schools', caption: 'Отзывы о школах' },
   schoolReview: {
     link: '/schools/[code]/reviews',
