7acd30d9f36fe23fa12508db7033a2eab4786360
Alexander Cheprasov <cheprasov.dev@gmail.com>
2022-09-20 19:08:26 +0300

    main: refactoring

--- src/containers/CourseCategories/index.jsx
+++ src/containers/CourseCategories/index.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 import CategoriesCard from 'components/CategoriesCard';
 import css from './index.module.scss';
 
-function CourseCategories({ categories, setVisibleDrawer }) {
+function CourseCategories({ categories, setVisibleDrawer, title, type }) {
   return (
-    <div className={css.container}>
-      <h2 className={css.mainTitle}>Направления курсов</h2>
+    <div>
+      <h2 className={css.mainTitle}>{title}</h2>
       <div className={css.wrapperList}>
         {categories.map(({ id, attributes }) => (
           <CategoriesCard
             key={id}
+            type={type}
+            category={attributes.code}
+            handler={setVisibleDrawer}
             caption={attributes.caption}
             subcategories={attributes.subcategories}
-            handler={setVisibleDrawer}
           />
         ))}
       </div>
