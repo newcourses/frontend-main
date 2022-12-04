1478dd217fd423f3a10778308bb2d6a193c2f54f
Alexander Cheprasov <cheprasov.dev@gmail.com>
2022-12-04 18:26:19 +0300

    dev: upgraded promo codes page

--- src/pages/promo-codes/index.jsx
+++ src/pages/promo-codes/index.jsx
@@ -1,13 +1,23 @@
 import Main from 'layouts/Main';
 import React from 'react';
 import useVisibleDrawer from 'hooks/useVisibleDrawer';
-import CategoriesServices from '../../api/services/categories';
+import CategoriesServices from 'api/services/categories';
+import NAVIGATION from 'library/navigation';
+import DynamicBreadcrumb from 'components/DynamicBreadcrumb';
+import ShowcasePromoCodes from 'containers/ShowcasePromoCodes';
+import PromoCodesServices from 'api/services/promo-codes';
 
-function PromoCodes({ categories }) {
+function PromoCodes({ categories, promoCodes }) {
   const { visibleDrawer, setVisibleDrawer } = useVisibleDrawer();
   const title = 'Действующие скидки и акции всех онлайн-школ 2022';
   const description =
     'Здесь мы собираем актуальные акции онлайн-школ с большими скидками. С этими акциями можно сильно сэкономить при покупке курсов';
+
+  const items = [
+    { value: 'home', navigation: NAVIGATION.home },
+    { value: 'schools', navigation: NAVIGATION.promoCodes },
+  ];
+
   return (
     <Main
       title={title}
@@ -16,19 +26,24 @@ function PromoCodes({ categories }) {
       visibleDrawer={visibleDrawer}
       setVisibleDrawer={setVisibleDrawer}
     >
-      <div>
-        <main>Скоро!</main>
-      </div>
+      <section>
+        <DynamicBreadcrumb items={items} />
+      </section>
+      <section>
+        <ShowcasePromoCodes promoCodes={promoCodes.data} />
+      </section>
     </Main>
   );
 }
 
 export async function getServerSideProps() {
   const categories = await CategoriesServices.getList();
+  const promoCodes = await PromoCodesServices.getList();
 
   return {
     props: {
       categories,
+      promoCodes,
     },
   };
 }
