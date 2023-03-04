ee71ea642076c16c224ffa382c5a3795c44e0f15
Alexander Cheprasov <cheprasov.dev@gmail.com>
2023-03-04 14:09:59 +0300

    MAIN-6: deleted TitleContainer

--- src/components/TitleContainer/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import css from './index.module.scss';
-
-function TitleContainer({ text }) {
-  return <h1 className={css.title}>{text}</h1>;
-}
-
-export default React.memo(TitleContainer);
