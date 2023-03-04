1ad29a1ee0a28ed45eea2339294388c99014e8a0
Alexander Cheprasov <cheprasov.dev@gmail.com>
2023-03-04 14:09:13 +0300

    MAIN-6: change Title component

--- src/containers/ShowcasePosts/index.jsx
+++ src/containers/ShowcasePosts/index.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-
 import PostCard from 'components/PostCard';
-import TitleContainer from 'components/TitleContainer';
+import Title from 'shared/ui/Title';
 import css from './index.module.scss';
 
 export default function ShowcasePosts({ posts }) {
   return (
     <>
-      <TitleContainer text="Блог" />
+      <Title text="Блог" />
       <div className={css.wrapperList}>
         {posts.map(({ attributes: post }) => (
           <PostCard
