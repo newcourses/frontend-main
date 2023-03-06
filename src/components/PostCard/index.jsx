ebc6e61fac8e272dfa7f0e00d9c019ffb36f2952
Alexander Cheprasov <cheprasov.dev@gmail.com>
2023-03-06 22:27:57 +0300

    MAIN-6: added background image, refactoring card body

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -1,37 +1,50 @@
 import React from 'react';
 import { Card } from 'antd';
 import Link from 'next/link';
+import moment from 'moment';
+import NAVIGATION from 'library/navigation';
+import css from './index.module.scss';
 
-export default function PostCard({
-  uid,
-  date,
-  title,
-  banner,
-  preview,
-  loading,
-}) {
+function PostCard({ uid, date, title, banner, preview, loading }) {
+  const backgroundImage = `url("${process.env.NEXT_PUBLIC_API}${
+    banner?.data?.attributes?.url || process.env.NEXT_PUBLIC_POST_PLUG
+  }")`;
+
   return (
-    <Link href={uid}>
+    <Link href={NAVIGATION.post.link} as={NAVIGATION.post.as(uid)}>
       <a>
         <Card
           loading={loading}
           hoverable
           cover={
-            <>
-              <img
-                alt="example"
-                src={`/api${
-                  banner?.data?.attributes?.url ||
-                  process.env.NEXT_PUBLIC_POST_PLUG
-                }`}
-              />
-              {date}
-            </>
+            <div className={css.coverWrapper}>
+              <div style={{ backgroundImage }} className={css.componentBanner}>
+                <div style={{ backgroundImage }} className={css.banner} />
+              </div>
+            </div>
           }
         >
-          <Card.Meta title={title} description={preview} />
+          <div className={css.wrapperBody}>
+            <Card.Meta
+              title={title}
+              description={preview}
+              style={{
+                height: '200px',
+                overflow: 'hidden',
+                'text-overflow': 'ellipsis',
+              }}
+            />
+            <div className={css.footer}>
+              <div className={css.date}>
+                {date && moment(date).format('DD.MM.YYYY')}
+              </div>
+              <div className={css.readCompletely}>Читать полностью</div>
+            </div>
+          </div>
         </Card>
       </a>
     </Link>
   );
 }
+
+export default PostCard;
