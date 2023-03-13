39c22fe53f442a3f82a7a782599f2aa4fee6797f
Alexander Cheprasov <cheprasov.dev@gmail.com>
2023-03-13 23:53:26 +0300

    MAIN-6: added EmptyPosts

--- src/containers/ShowcasePosts/index.jsx
+++ src/containers/ShowcasePosts/index.jsx
@@ -2,22 +2,28 @@ import React from 'react';
 import PostCard from 'components/PostCard';
 import Title from 'shared/ui/Title';
 import css from './index.module.scss';
+import EmptyPosts from "./EmptyPosts";
 
-export default function ShowcasePosts({ posts }) {
+export default function ShowcasePosts({ posts, setVisibleDrawer }) {
   return (
     <>
       <Title text="Блог" />
-      <div className={css.wrapperList}>
-        {posts.map(({ attributes: post }) => (
-          <PostCard
-            uid={post.uid}
-            title={post.title}
-            date={post.date}
-            preview={post.preview}
-            banner={post.banner}
-          />
-        ))}
-      </div>
+      {posts.length ? (
+        <div className={css.wrapperList}>
+          {posts.map(({ attributes: post }) => (
+            <PostCard
+              uid={post.uid}
+              title={post.title}
+              date={post.date}
+              preview={post.preview}
+              banner={post.banner}
+            />
+          ))}
+          )
+        </div>
+      ) : (
+        <EmptyPosts setVisibleDrawer={setVisibleDrawer} />
+      )}
     </>
   );
 }
